9cd4fc93c9383e49efa665938a7ad17779dfc2a0
ccarver80 <ckc_80@outlook.com>
2022-02-06 11:01:50 -0700

    Built routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+// Components
 import Header from "./Components/Header";
+import Home from "./Components/Home"
+import AboutMe from "./Components/AboutMe";
+import Projects from './Components/Projects';
+import Blog from './Components/Blog';
+
 
 function App() {
   return (
-    <div>
-   <Header /> 
-   <h1>Carver.Codes</h1>
-   <h2>Comming Soon!</h2>
-   <p>Updates: 2/26/2022: Started repo, created react app, set up netlify deployment and deployed the begining stages.</p>
-   </div>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/about' element={<AboutMe />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/blog' element={<Blog />} /> 
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ front-end/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Carver.Codes COMMING SOON!</h1>
+                <h1>Updates:</h1>
+                    <h2>02/06/2022</h2>
+                        <p>Built main repository, created react app called "front-end" and got linked to netlify for deployment. Started building the outline of the website</p>
+                        
+        </div>
+    )
+}
+
+export default Home; 
