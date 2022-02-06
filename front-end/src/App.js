10227a16cc0d8df5be24267db58d0df03ca774bf
ccarver80 <ckc_80@outlook.com>
2022-02-06 09:29:26 -0700

    Netlify deploy enabled, ready to start building front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,13 @@
-
+import Header from "./Components/Header";
 
 function App() {
   return (
+    <div>
+   <Header /> 
    <h1>Carver.Codes</h1>
+   <h2>Comming Soon!</h2>
+   <p>Updates: 2/26/2022: Started repo, created react app, set up netlify deployment and deployed the begining stages.</p>
+   </div>
   );
 }
 
