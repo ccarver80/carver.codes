02b97883045bc4b3d6e4ae9690f22630764b46ad
ccarver80 <ckc_80@outlook.com>
2022-02-09 18:37:34 -0700

    Got three planets working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {Canvas} from '@react-three/fiber'
-import {OrbitControls, Stars, Html, Scroll, ScrollControls} from '@react-three/drei'
+import {OrbitControls, Stars, Html, Scroll, ScrollControls, ContactShadows, FlyControls, Billboard} from '@react-three/drei'
 import './Style/style.css'
 
 // Components
@@ -12,27 +12,64 @@ import Projects from './Components/Projects';
 import Blog from './Components/Blog';
 import NotFound from './Components/NotFound';
 
-function Box() {
+function Sun() {
   return (
-    <mesh>
+    <mesh scale={10} position={[0, 0, 0]}>
       <sphereBufferGeometry attach='geometry' />
-      <meshLambertMaterial attach='material' color='blue' />
+      <meshLambertMaterial attach='material' color='yellow' />
     </mesh>
   )
 }
 
+function Planet1() {
+  return (
+    <mesh scale={3} position={[-5, 0, 200]}>
+      <sphereBufferGeometry attach='geometry' />
+      <meshLambertMaterial attach='material' color='red' />
+    </mesh>
+  )
+}
+
+function Planet2() {
+  return (
+    <mesh scale={3}  position={[5, 0, 150]}>
+      
+      <sphereBufferGeometry attach='geometry'/>
+      <meshLambertMaterial attach='material' color='green' />
+
+     
+    </mesh>
+  )
+}
+
+function Planet3() {
+  return (
+    <mesh scale={3} position={[-5, 0, 100]}>
+      
+      <sphereBufferGeometry attach='geometry'/>
+      <meshLambertMaterial attach='material' color='purple' />
+
+     
+    </mesh>
+  )
+}
+
+
+
 function App() {
   return ( 
-  <Canvas>
+  <Canvas camera={{position: [0,0,250]}} >
     
-    <ambientLight intensity={.5}/>
-    <spotLight position={[10,10,10]} />
-      <Html>
-        <p>Hello</p>
-      </Html>
-    <OrbitControls/> 
-    <Box position={[0,10,0]} />
-    <Stars />
+    <ambientLight intensity={0.2} />
+    <spotLight intensity={10} position={[0,0,40]} />
+    <ContactShadows />
+    <FlyControls movementSpeed={50} rollSpeed={0}  />
+    <pointLight intensity={10} position={[-10, 0, 0]} />
+    <Planet3 />
+    <Planet2 />
+    <Planet1 />
+    <Sun />
+    <Stars radius={100} count={10000}  />
   </Canvas>
   );
 }
