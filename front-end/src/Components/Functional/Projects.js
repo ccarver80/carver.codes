5092bfa020aeb868b7d2c6d100aa597656529780
ccarver80 <ckc_80@outlook.com>
2022-03-16 18:41:49 -0600

    trying to add pagination

--- front-end/src/Components/Functional/Projects.js
+++ front-end/src/Components/Functional/Projects.js
@@ -1,12 +1,9 @@
-import React, { Suspense, useState, useEffect,} from "react";
-import {useNavigate} from 'react-router-dom'
-import { Canvas, } from "@react-three/fiber";
+import React, { Suspense, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
 import {
   Billboard,
-  Html,
-  OrbitControls,
-  Scroll,
-  ScrollControls,
+  Html, 
   useTexture,
 } from "@react-three/drei";
 
@@ -16,7 +13,6 @@ import TimeofDay from "../Supportive/TImeofDay";
 import mars from "../Imgs/planetTexture/Martian.png";
 
 import testingAPI from "../../api";
-
 
 function Plane() {
   const Mars = useTexture(mars);
@@ -28,57 +24,74 @@ function Plane() {
   );
 }
 
-
 // =====CREATE A FETCH REQUEST TO SERVER TO GET TITLES OF ALL PROJECTS ====//
 
-
-
 const Projects = () => {
-
   const [projects, setProjects] = useState([]);
+  const [pages, setPages] = useState();
+  const pageNumbers = [];
 
   useEffect(() => {
-
-    const fetchProjects = async () => { 
+    const fetchProjects = async () => {
       await fetch(testingAPI + "api/projects")
         .then((res) => res.json())
-        .then((data) => setProjects(data));
+        .then((data) => {
+          setProjects(data);
+          setPages(data.length / 3);
+        });
     };
 
     fetchProjects();
+    
   }, []);
 
 
-  const nav = useNavigate()
+  for (let i = 1; i <= Math.ceil(pages); i++) {
+    pageNumbers.push(i);
+  }
+
+  
+  function pageClick(range) {
+    console.log(range)
+  }
+
+  const nav = useNavigate();
   return (
     <Suspense fallback={<Loading />}>
       <Canvas camera={{ position: [30, 0, 0] }}>
         <ambientLight intensity={0.5} />
 
-        
         <Billboard position={[0, 0, 0]}>
-  <Html transform={true}>
-    <div className="projects">
-    <h3>Click on a project to learn more!</h3>
-    {projects ?
-                 projects.map((project) => (
-                   <div className="blogList">
-                    <a href={"/project/" + project.id}>
-                      <h1>{project.title}</h1>
-                      </a>
-                    </div>
-                  ))
-                  : <h1>Loading projects....</h1>
-                }
-      {/* Button back to space */}
-       <div><button onClick={()=> {
-                  nav('/')
-                }} className="enter-button">Back to space!</button></div>
-    </div>
-   
-  </Html>
-</Billboard>
-      
+          <Html transform={true}>
+            <div className="projects">
+              <h3>Click on a project to learn more!</h3>
+              {projects.map((project) => (
+                <div className="blogList">
+                  <a href={"/project/" + project.id}>
+                    <h1>{project.title}</h1>
+                  </a>
+                  <p>{project.desc}</p>
+                </div>
+              ))}
+              <h3>Page:</h3>
+              {pageNumbers.map((page) => (
+                <button  className="pageNumber">{page}</button>
+              ))}
+              {/* Button back to space */}
+              <div>
+                <button
+                  onClick={() => {
+                    nav("/");
+                  }}
+                  className="enter-button"
+                >
+                  Back to space!
+                </button>
+              </div>
+            </div>
+          </Html>
+        </Billboard>
+
         <Plane />
         <TimeofDay />
       </Canvas>
